74bd887b5695bfa6ce0cf72fa24b6c56212ecb40
Remy <remypaak@hotmail.com>
2024-06-25 22:57:49 +0200

    Add member functionality in UI

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,9 +6,10 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([errorInterceptor])), provideAnimations(), provideToastr({
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])), provideAnimations(), provideToastr({
     positionClass: 'toast-bottom-right'
   })],
 };
